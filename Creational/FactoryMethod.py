b1a68e52176a85ee616ff9e9115a974f48aff729
Enes K. <eneskuzu2003@gmail.com>
2025-05-06 16:22:04 +0300

    FactoryMethod eklendi.

---
+++ Creational/FactoryMethod.py
@@ -0,0 +1,28 @@
+# Ürün sınıfları
+class Kedi:
+    def konus(self):
+        return "Miyav"
+
+class Kopek:
+    def konus(self):
+        return "Hav hav"
+
+# Factory method
+def hayvan_olustur(tur):
+    if tur == "kedi":
+        return Kedi()
+    elif tur == "kopek":
+        return Kopek()
+    else:
+        raise ValueError("Bilinmeyen hayvan türü!")
+
+hayvan1 = hayvan_olustur("kedi")
+hayvan2 = hayvan_olustur("kopek")
+
+print(hayvan1.konus())  # Miyav
+print(hayvan2.konus())  # Hav hav
+
+#Alt sınıfların hangi nesneyi oluşturacağına karar vermesini sağlar; nesne oluşturma süreci bir metotla soyutlanır.
+#hayvan_olustur() fonksiyonu bir factory method gibi çalışarak doğru sınıfın nesnesini üretir.
+#Bu desen sayesinde nesne oluşturma işlemi koddan soyutlanır ve genişletilebilir hâle gelir.
+
